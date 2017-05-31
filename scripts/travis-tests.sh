e041a504cbd6dc2fe04578c5cb4970b268459aa0
Brian Telnes <btelnes@microsoft.com>
2017-05-31 22:49:44 +0000

    Update Travis tests to compile assets

--- scripts/travis-tests.sh
+++ scripts/travis-tests.sh
@@ -1,1 +1,6 @@
-echo "Testing edx-theme"
+sudo -H -u edxapp bash
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+
+# Updates requirements and compiles javascript, Sass and CoffeeScript
+paver update_assets
--- travis-ci/scripts/compile-assets.sh
+++
@@ -1,6 +0,0 @@
-sudo -H -u edxapp bash
-source /edx/app/edxapp/edxapp_env
-cd /edx/app/edxapp/edx-platform
-
-# Updates requirements and compiles javascript, Sass and CoffeeScript
-paver update_assets
