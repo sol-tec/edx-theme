1972fc8ff92adce9fc4211a431cea7d288456e05
Brian Telnes <btelnes@microsoft.com>
2017-06-01 18:38:20 +0000

    Update .travis.yml file to install paver and dependencies

--- scripts/travis-tests.sh
+++ scripts/travis-tests.sh
@@ -1,6 +1,8 @@
-sudo -H -u edxapp bash
-source /edx/app/edxapp/edxapp_env
-cd /edx/app/edxapp/edx-platform
+#sudo -H -u edxapp bash
+#source /edx/app/edxapp/edxapp_env
+#cd /edx/app/edxapp/edx-platform
 
 # Updates requirements and compiles javascript, Sass and CoffeeScript
 paver update_assets
+
+
