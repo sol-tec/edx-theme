34d6f71ab1a807488b713f30ec078b974368930f
Brian Telnes <btelnes@microsoft.com>
2017-06-01 21:49:24 +0000

    Update tests file

--- scripts/travis-tests.sh
+++ scripts/travis-tests.sh
@@ -7,13 +7,9 @@
 #fi
 
 #sudo -H -u edxapp bash
-#source /edx/app/edxapp/edxapp_env
-#cd /edx/app/edxapp/edx-platform
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
 
 # Updates requirements and compiles javascript, Sass and CoffeeScript
-#paver update_assets
-
-
-sudo -H bash
-
+paver update_assets
 
