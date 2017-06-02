7fc1922dca9fb895daac65660ca80955fda105c6
Brian Telnes <btelnes@microsoft.com>
2017-06-02 17:37:08 +0000

    Update tests file

--- scripts/travis-tests.sh
+++ scripts/travis-tests.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #if ! id -u edxapp > /dev/null 2>&1; then
 #  # create required edxapp user account
 #  sudo adduser --disabled-password --gecos "" edxapp
@@ -8,8 +10,8 @@
 
 #sudo -H -u edxapp bash
 source /edx/app/edxapp/edxapp_env
-cd /edx/app/edxapp/edx-platform
+#cd /edx/app/edxapp/edx-platform
 
 # Updates requirements and compiles javascript, Sass and CoffeeScript
-paver update_assets
+#paver update_assets
 
