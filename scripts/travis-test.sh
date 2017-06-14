0be975691f3ac71c2919ec461b103a6fffbbdbae
Brian Telnes <btelnes@microsoft.com>
2017-06-14 17:39:23 +0000

    Update to deploy DevStack

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -9,7 +9,7 @@
 #fi
 
 #sudo -H -u edxapp bash
-source /edx/app/edxapp/edxapp_env
+#source /edx/app/edxapp/edxapp_env
 #cd /edx/app/edxapp/edx-platform
 
 # Updates requirements and compiles javascript, Sass and CoffeeScript
