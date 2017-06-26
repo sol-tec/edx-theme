9ddc4e21c73d323fcf214a491c94b00bc166ac5d
Brian Telnes <btelnes@microsoft.com>
2017-06-26 14:41:20 -0700

    Try to use container image from DockerHub

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-#if ! id -u edxapp > /dev/null 2>&1; then
-#  # create required edxapp user account
-#  sudo adduser --disabled-password --gecos "" edxapp
-#
-#  # set the edxapp password
-#  #sudo usermod --password P@$$sw0rd edxapp
-#fi
-
-#sudo -H -u edxapp bash
-#source /edx/app/edxapp/edxapp_env
-#cd /edx/app/edxapp/edx-platform
+sudo -H -u edxapp bash
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
 
 # Updates requirements and compiles javascript, Sass and CoffeeScript
-#paver update_assets
+paver update_assets
 
