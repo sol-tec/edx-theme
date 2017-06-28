61843eec206de016543f17ddcc8c6dfe2fece966
Brian Telnes <btelnes@microsoft.com>
2017-06-28 12:38:04 -0700

    Try to use container image from DockerHub

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker exec -it devstack /bin/bash -s <<EOF
+docker exec -i devstack /bin/bash -s <<EOF
 sudo su edxapp
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
