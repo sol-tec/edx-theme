0ed9849b3fc672d6a5cbaf46a1ec9737878801ab
Brian Telnes <btelnes@microsoft.com>
2017-06-28 15:46:37 -0700

    Try to use container image from DockerHub

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -5,5 +5,5 @@ sudo su edxapp
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
 # Updates requirements and compiles javascript, Sass and CoffeeScript
-paver update_assets --settings devstack_docker
+paver update_assets --settings docker_devstack
 EOF
