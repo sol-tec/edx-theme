f0122ad8efe2f356a72ca41a364ee8219ecc774f
Brian Telnes <btelnes@microsoft.com>
2017-06-28 19:10:45 -0700

    Try to use container image from DockerHub

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker exec -i devstack /bin/bash -s <<EOF
-git clone -b oxa/devfic https://github.com/Microsoft/edx-theme.git /tmp/edx-theme
+mkdir /tmp/edx-theme
+cd /tmp/edx-theme
+sudo git clone -b oxa/devfic https://github.com/Microsoft/edx-theme.git
 cp -r -u /tmp/edx-theme /edx/app/edxapp/themes
 sudo su edxapp
 source /edx/app/edxapp/edxapp_env
