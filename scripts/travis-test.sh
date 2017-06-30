974d130ff5a46ad6f7a8f41c5fbb9e06ce242f61
Brian Telnes <btelnes@microsoft.com>
2017-06-30 14:18:00 -0700

    Try to use container image from DockerHub

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 docker exec -i devstack /bin/bash -s <<EOF
-#export XDG_RUNTIME_DIR=/run/user/1000
-
 # Ensure that MySql is running.  Fixes error "Can't connect to MYSQL server on '127.0.0.1' (111)"
 UP=$(/etc/init.d/mysql status | grep running | grep -v not | wc -l);
-if [[ "$UP" -ne "1" ]]; then echo "Starting MySQL..."; systemctl start mysql.service; sleep 10; fi
+if [[ "$UP" -ne "1" ]]; then echo "Starting MySQL..."; systemctl start mysql.service; fi
+
+# Switch to the edxapp user
 sudo su edxapp
-cd /edx/app/edxapp
-rm -r -d -f themes
 
 # Get the latest theme files
+cd /edx/app/edxapp
+rm -r -d -f themes
 git clone -b oxa/devfic https://github.com/Microsoft/edx-theme.git themes
+
+# Source the edxapp_env file to set various required environment variables
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
 
